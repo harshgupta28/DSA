141f6a99ef908550a6cb58e4895a3e1d337e58dd
Harsh Gupta <hg.harsh28@gmail.com>
2021-04-22 19:03:44 +0530

    Add files via upload

--- Implementation/Trie.java
+++ Implementation/Trie.java
@@ -1,102 +1,96 @@
-// Java implementation of search and insert operations
-// on Trie
-public class Trie {
+import java.util.HashMap;
+import java.util.Map;
 
-	// Alphabet size (# of symbols)
-	static final int ALPHABET_SIZE = 26;
-
-	// trie node
-	static class TrieNode {
-		TrieNode[] children = new TrieNode[ALPHABET_SIZE];
-
-		// isEndOfWord is true if the node represents
-		// end of a word
-		boolean isEndOfWord;
-
-		TrieNode() {
-			isEndOfWord = false;
-			for (int i = 0; i < ALPHABET_SIZE; i++)
-				children[i] = null;
-		}
-	};
-
-	static TrieNode root;
-
-	// If not present, inserts key into trie
-	// If the key is prefix of trie node,
-	// just marks leaf node
-	static void insert(String key) {
-		int level;
-		int length = key.length();
-		int index;
-
-		TrieNode pCrawl = root;
-
-		for (level = 0; level < length; level++) {
-			index = key.charAt(level) - 'a';
-			if (pCrawl.children[index] == null)
-				pCrawl.children[index] = new TrieNode();
-
-			pCrawl = pCrawl.children[index];
-		}
-
-		// mark last node as leaf
-		pCrawl.isEndOfWord = true;
-	}
-
-	// Returns true if key presents in trie, else false
-	static boolean search(String key) {
-		int level;
-		int length = key.length();
-		int index;
-		TrieNode pCrawl = root;
-
-		for (level = 0; level < length; level++) {
-			index = key.charAt(level) - 'a';
-
-			if (pCrawl.children[index] == null)
-				return false;
-
-			pCrawl = pCrawl.children[index];
-		}
-
-		return (pCrawl != null && pCrawl.isEndOfWord);
-	}
-
-	// Driver
-	public static void main(String args[]) {
-		// Input keys (use only 'a' through 'z' and lower case)
-		String keys[] = {"the", "a", "there", "answer", "any",
-		                 "by", "bye", "their"
-		                };
-
-		String output[] = {"Not present in trie", "Present in trie"};
-
-
-		root = new TrieNode();
-
-		// Construct trie
-		int i;
-		for (i = 0; i < keys.length ; i++)
-			insert(keys[i]);
-
-		// Search for different keys
-		if (search("the") == true)
-			System.out.println("the --- " + output[1]);
-		else System.out.println("the --- " + output[0]);
-
-		if (search("these") == true)
-			System.out.println("these --- " + output[1]);
-		else System.out.println("these --- " + output[0]);
-
-		if (search("their") == true)
-			System.out.println("their --- " + output[1]);
-		else System.out.println("their --- " + output[0]);
-
-		if (search("thaw") == true)
-			System.out.println("thaw --- " + output[1]);
-		else System.out.println("thaw --- " + output[0]);
-
-	}
+public class Trie
+{
+	 private class TrieNode
+	 {
+	 	Map<Character, TrieNode> children;
+	 	boolean endOfWord;
+	 	public TrieNode()
+	 	{
+	 		children = new HashMap<Character, TrieNode>() ;
+	 		endOfWord = false ;
+	 	}
+	 	
+	 }
+	 
+	 private final TrieNode root ;
+	 
+	 public Trie()
+	 {
+		 root = new TrieNode();
+	 }
+	 
+	 public void insert(String word)
+	 {
+		 TrieNode current = root;
+		 for(int i=0;i<word.length();i++)
+		 {
+			 char ch = word.charAt(i);
+			 TrieNode node = current.children.get(ch);
+			 if(node == null)
+			 {
+				 node = new TrieNode();
+				 current.children.put(ch, node);
+			 }
+			 
+			 current = node ;
+		 }
+		 
+		 current.endOfWord = true;
+	 }
+	 
+	 public boolean search(String word)
+	 {
+		 TrieNode current = root;
+		 for(int i=0;i<word.length();i++)
+		 {
+			 char ch = word.charAt(i);
+			 TrieNode node = current.children.get(ch);
+			 if(node == null)
+				 return false;	
+			 
+			 current = node ;
+		 }
+		 
+		 return current.endOfWord ;
+	 }
+	 
+	 public void delete (String word)
+	 {
+		 TrieNode current = root;
+		 for(int i=0;i<word.length();i++)
+		 {
+			 char ch = word.charAt(i);
+			 TrieNode node = current.children.get(ch);
+			 if(node == null)
+				 return ;	
+			 
+			 current = node ;
+		 }
+		 
+		 current.endOfWord = false ;
+	 }
+	 
+	 
+	 public static void main(String[] args) 
+	 {
+		Trie trie = new Trie();
+		
+		trie.insert("abcd");
+		trie.insert("lmn");
+		trie.insert("abcderg");
+		
+		System.out.println(trie.search("abcd"));
+		System.out.println(trie.search("abc"));
+		System.out.println(trie.search("lmn"));
+		System.out.println(trie.search("abcderg"));
+		
+		trie.delete("abcderg");
+		System.out.println(trie.search("abcderg"));		
+		
+	 }
+	 
 }
-// This code is contributed by Sumit Ghosh
+
